246822e9d77549ec78c994c0be56721834ce4eb6
hoverlover <hoverlover@gmail.com>
2012-06-21 03:19:10 -0500

    Use Mongolid instead of MongoMapper.

--- lib/product_selector.rb
+++ lib/product_selector.rb
@@ -1,14 +1,14 @@
+require "mongoid"
 require "product_selector/version"
-require "mongo_mapper"
 
 require "product_selector/answer"
 require "product_selector/category"
 require "product_selector/question"
 require "product_selector/survey"
 require "product_selector/user"
+Mongoid.logger.level = :info
+Mongoid.load! "mongoid.yml"
 
 module ProductSelector
 end
 
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "product_selector"
